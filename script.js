919e7a2d33ce102349a620f1a6ddd791388b5631
iamricky17 <patrickzablon0@gmail.com>
2024-12-07 14:55:23 +0300

    Added some functionalities and improved UI

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function changeBackground() {
     const body = document.body;
-  
-    if (body.style.backgroundImage.includes("pic_bulbon")) {
-      body.style.backgroundImage = "url('pic_bulboff.jpg')";
+
+    if (body.classList.contains("bulbon")) {
+        body.classList.remove("bulbon");
+        body.classList.add("bulboff");
     } else {
-      body.style.backgroundImage = "url('pic_bulbon.jpg')";
+        body.classList.remove("bulboff");
+        body.classList.add("bulbon");
     }
-  }
-  
-  // Add click event to the entire body
-  document.body.addEventListener("click", changeBackground);
-  
+}
+
+// Add click event to the entire body
+document.body.addEventListener("click", changeBackground);
